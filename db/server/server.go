01bef54c74686026a3ef90c2c93c003e035cd03d
Graham Albachten <albachteng@gmail.com>
2023-04-22 15:53:36 -0500

    second and third pass

--- db/server/server.go
+++ db/server/server.go
@@ -1,7 +1,12 @@
 package server
 
-import "os"
+import (
+	"fmt"
+	"math/rand"
+	"os"
+)
 
+// issue: what if there is a problem during the write?
 func SaveData1(path string, data []byte) error {
 	f, err := os.OpenFile(path, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0664)
 	if err != nil {
@@ -10,4 +15,40 @@ func SaveData1(path string, data []byte) error {
 	defer f.Close()
 	_, err = f.Write(data)
 	return err
+}
+
+func SaveData2(path string, data []byte) error {
+	tmp := fmt.Sprintf("%s.tmp.%d", path, rand.Int())
+	f, err := os.OpenFile(tmp, os.O_WRONLY|os.O_CREATE|os.O_EXCL, 0664)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+	_, err = f.Write(data)
+	if err != nil {
+		os.Remove(tmp) // remove the file if something went wrong while writing
+		return err
+	}
+	return os.Rename(tmp, path) // rename is atomic
+}
+
+// flush the file system before committing
+func SaveData3(path string, data []byte) error {
+	tmp := fmt.Sprintf("%s.tmp.%d", path, rand.Int())
+	f, err := os.OpenFile(tmp, os.O_WRONLY|os.O_CREATE|os.O_EXCL, 0664)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+	_, err = f.Write(data)
+	if err != nil {
+		os.Remove(tmp)
+		return err
+	}
+	err = f.Sync() // "fsync" or equivalent syscall
+	if err != nil {
+		os.Remove(tmp)
+		return err
+	}
+	return os.Rename(tmp, path)
 }
